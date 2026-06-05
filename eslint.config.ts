4f0a881da9bcd97cd68546b8be4a4f6e110585ab
Chemaclass <chemaclass@outlook.es>
2026-06-05 10:17:06 +0200

    chore: exclude scripts and config files from eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,7 +2,7 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist', 'node_modules', '*.config.ts'] },
+  { ignores: ['dist', 'node_modules', '*.config.ts', 'scripts/**'] },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   {
